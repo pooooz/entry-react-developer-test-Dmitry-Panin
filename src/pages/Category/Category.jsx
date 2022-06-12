081ac224cb32c3f9bc0f7dd284726798a94a137c
POZ <antoivans@gmail.com>
2022-06-12 20:30:21 +0300

    Added displaying of items by category

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Query } from '@apollo/client/react/components';
 
+import { GET_PRODUCTS_BY_CATEGORY } from 'queries/products';
+import { ProductList } from './components/ProductList/ProductList';
 import styles from './Category.module.scss';
 
 export class Category extends React.Component {
@@ -7,6 +10,16 @@ export class Category extends React.Component {
     return (
       <section className={styles.category}>
         <h1 className={styles.category__header}>{this.props.category}</h1>
+        <section>
+          <Query query={GET_PRODUCTS_BY_CATEGORY(this.props.category)}>
+            {({ loading, error, data }) => {
+              if (error) return <h1>Error...</h1>;
+              if (loading || !data) return <h1>Loading...</h1>;
+
+              return <ProductList products={data.category.products} />;
+            }}
+          </Query>
+        </section>
       </section>
     );
   }
